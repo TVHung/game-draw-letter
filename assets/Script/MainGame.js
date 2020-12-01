4b8885d5cc3c881973aa5f2477e5fbdac443c9e4
truong hung <truonghungvatvo@gmail.com>
2020-12-01 20:12:54 +0700

    sua image

--- assets/Script/MainGame.js
+++ assets/Script/MainGame.js
@@ -33,17 +33,3 @@ cc.Class({
 
     // update (dt) {},
 });
-// if(this.apples[arr[2][this.letterIndex].split(" ")[this.nextStep - 1]].active === false && checkNext === true && this._isGameOver === false){
-//     setTimeout(()=>{
-//         this.brush.getComponent('Brush').clear();
-//         if(this.nextStep === 2)
-//             this.net1.getComponent(cc.Animation).play();
-//         if(this.nextStep === 3)
-//             this.net2.getComponent(cc.Animation).play();
-//         this.tutorialScript.string = "Bạn vẽ đúng rồi vẽ nét thứ " + (this.nextStep) + " nhé!";
-//     }, 500);
-//     setTimeout(()=>{
-//         this.tutorial.getComponent(cc.Animation).play('net'+ (this.nextStep) + '_A');
-//     }, 1500);
-//     this.nextStep = this.nextStep + 1;
-// }
